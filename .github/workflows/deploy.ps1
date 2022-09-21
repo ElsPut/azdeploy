b8e64cdb4844d625c8705945dd05a59d9811ef8f
ElsPut <els@u2u.be>
2022-09-21 14:51:34 +0200

    v13

--- .github/workflows/deploy.ps1
+++ .github/workflows/deploy.ps1
@@ -1,6 +1,5 @@
 az login
 
-az ad sp create-for-rbac --name spgitaccount --role contributor
- --scopes /subscriptions/a39a4f77-4c6c-42b3-a9a1-1f96a9842799 --sdk-auth
+az ad sp create-for-rbac --name spgitaccount --role contributor --scopes /subscriptions/f2214158-e53d-4a65-abc0-9faccc3b3127 --sdk-auth
 
 az account list
